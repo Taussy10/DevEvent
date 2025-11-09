e1100439e07af3be3c3bc5cdab4d0b526b5ae8ae
Tausif <tausif00100@gmail.com>
2025-11-09 11:54:03 +0530

    added light rays

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from "react";
+// import LightRays from "./LightRays";
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section className="">
+      {/* Why H1 getting gradient cause in global styles we applied gradient */}
+      <h1 className="text-center">
+        The Hub for Every Dev
+        {/* br is for moving to next line */}
+        <br />
+        Event You Can't Miss
+      </h1>
+      <p className=" text-center mt5 ">Hackathons, Meetups and Conferences, All in One Place</p>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
