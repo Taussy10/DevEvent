24825c5b60d2edf222f87514532f7a4148616f3e
Tausif <tausif00100@gmail.com>
2025-11-13 00:36:21 +0530

    ccorect index page

--- database/index.ts
+++ database/index.ts
@@ -7,5 +7,3 @@ export { default as Event, IEvent } from "./event.model";
 export { default as Booking, IBooking } from "./booking.model";
 
 // Typescript interfaces exports
-export type { IEvent } from "@/database/event.model";
-export type { IBooking } from "@/database/booking.model";
